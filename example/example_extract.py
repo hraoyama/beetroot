05a15e3d14c87f4fc320e846525290c59be37306
hraoyama <hraoyama@gmail.com>
2020-06-21 01:50:52 +0100

    Add files via upload

--- example/example_extract.py
+++ example/example_extract.py
@@ -10,20 +10,18 @@ from qpython import qconnection
 import multiprocessing
 import logging
 from logging.handlers import QueueHandler, QueueListener
-from subprocess import Popen
 import time
 
-
-TRADES_DIR =  "I:/testkdb/trades/" # "I:/beetroot/trades/"  # where KDB trades will live  # "I:/testkdb/trades/" #
-BOOKS_DIR = "I:/testkdb/books/"  # "I:/beetroot/books/"  # where KDB books will live
+TRADES_DIR = "I:/beetroot/trades/"  # where KDB trades will live  # "I:/testkdb/trades/" #
+BOOKS_DIR = "I:/beetroot/books/"  # where KDB books will live "I:/testkdb/books/"  #
 EXTRACT_DIR = "I:/beetroot/csv_data_from_py"  # where we temporarily extract files and delete them after processing
 LOG_DIR = "D:/data/logs/"  # where we write the logs of the process
 Q_DIR = "D:/q/w64"  # where the q executable lives
-N_Q_THREADS = 12 # the number of threads you want Q to run in
+N_Q_THREADS = 12  # the number of threads you want Q to run in
+
 
 # a logger that handles logging from multiple processes
 def logger_init():
-
     mp_q = multiprocessing.Queue()
     # this is the handler for all log records
     handler = logging.StreamHandler()
@@ -33,15 +31,15 @@ def logger_init():
     ql = QueueListener(mp_q, handler)
     ql.start()
     logger = logging.getLogger()
-
+    
     logger.setLevel(logging.DEBUG)
     # add the handler to the logger so records from this process are handled
     logger.addHandler(handler)
-
+    
     return ql, mp_q
 
+
 def clean_up_files(copied_original, extracted_file, full_output_filename):
-    
     # if passed_check:
     os.remove(f'{extracted_file}')
     if os.path.isfile(extracted_file):
@@ -55,15 +53,16 @@ def clean_up_files(copied_original, extracted_file, full_output_filename):
         else:
             logging.info(f'Deleted {full_output_filename}')
 
+
 def date_string_from_file_name(filename):
     return datetime.strptime(os.path.basename(filename).split("_")[0], '%Y%m%d').strftime("%Y.%m.%d")
 
+
 def process_book_file(q, zipped_book_filename, kdb_books_dir=BOOKS_DIR, parent_dir=None, temp_dir=None):
-    
-    full_input_filename = zipped_book_filename if parent_dir is None else os.path.join(parent_dir,zipped_book_filename).replace("\\", "/")
+    full_input_filename = zipped_book_filename if parent_dir is None else os.path.join(parent_dir, zipped_book_filename).replace("\\", "/")
     
     # if no output dir is specified, file will be extracted in place
-    full_output_filename = None if temp_dir is None else os.path.join(temp_dir, zipped_book_filename).replace("\\","/")
+    full_output_filename = None if temp_dir is None else os.path.join(temp_dir, zipped_book_filename).replace("\\", "/")
     
     copied_original = False
     if full_output_filename is not None:
@@ -84,22 +83,22 @@ def process_book_file(q, zipped_book_filename, kdb_books_dir=BOOKS_DIR, parent_d
     
     date_string = date_string_from_file_name(extracted_file)
     logging.info(f"Processing {extracted_file}")
-
+    
     q(f'books:("SDIIFFFFFFFFFFFFFFFFFFFF";enlist "|") 0: `:{extracted_file};')
     q('update sym:ISIN, time: (1000*TimeMM) + Date + `second$(60*60*TimeSec div 10000) + (60 * (TimeSec mod 10000) div 100) + (TimeSec mod 100) from `books;')
     q('delete from `books where Bid_Px_Lev_0<=0;delete ISIN, TimeMM, Date, TimeSec, MarketTime from `books;')
     q(f'books:`sym`time xcols books;')
     q(f'.Q.dpft[`:{kdb_books_dir};{date_string};`sym;`books];')
-
-    clean_up_files(copied_original, extracted_file, full_output_filename)
     
+    clean_up_files(copied_original, extracted_file, full_output_filename)
+
 
 def process_trade_file(q, zipped_trade_filename, kdb_trades_dir=TRADES_DIR, parent_dir=None, temp_dir=None,
                        add_check=True):
-    full_input_filename = zipped_trade_filename if parent_dir is None else os.path.join(parent_dir,zipped_trade_filename).replace("\\", "/")
+    full_input_filename = zipped_trade_filename if parent_dir is None else os.path.join(parent_dir, zipped_trade_filename).replace("\\", "/")
     
     # if no output dir is specified, file will be extracted in place
-    full_output_filename = None if temp_dir is None else os.path.join(temp_dir, zipped_trade_filename).replace("\\","/")
+    full_output_filename = None if temp_dir is None else os.path.join(temp_dir, zipped_trade_filename).replace("\\", "/")
     
     copied_original = False
     if full_output_filename is not None:
@@ -141,8 +140,8 @@ def process_trade_file(q, zipped_trade_filename, kdb_trades_dir=TRADES_DIR, pare
     #             f"Failed to upload to KDB {full_input_filename} => {full_output_filename} => {extracted_file}")
     #         passed_check = False
 
+
 def process_month_books(logger, month_pattern, data_path, temp_dir=EXTRACT_DIR):
-    
     qh = QueueHandler(logger)
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
@@ -166,7 +165,7 @@ def process_month_books(logger, month_pattern, data_path, temp_dir=EXTRACT_DIR):
         logging.info('IPC version: %s. Is connected: %s' % (q.protocol_version, q.is_connected()))
         
         book_pattern = re.compile("^" + month_pattern + "[0-9]{2}_\S+_Book.csv.gz$")
-
+        
         # make sure the file patterns are mutually exclusive, else you will be loading multiple times the same data
         book_gz_csv_files = [x for x in os.listdir(data_path) if book_pattern.match(x) is not None]
         if not book_gz_csv_files:
@@ -176,18 +175,19 @@ def process_month_books(logger, month_pattern, data_path, temp_dir=EXTRACT_DIR):
         logging.info(f"Found book files {str(book_gz_csv_files)}")
         
         for zipped_book_filename in book_gz_csv_files:
-            process_book_file(q, zipped_book_filename, parent_dir=data_path, temp_dir=temp_dir, kdb_trades_dir=BOOKS_DIR)
-            
+            process_book_file(q, zipped_book_filename, parent_dir=data_path, temp_dir=temp_dir, kdb_books_dir=BOOKS_DIR)
+    
     finally:
+        # stop q process
+        q(f'\\')
+        logging.info(f'{month_pattern} stopped Q Process on port {port_num}.')
         # close connection
         q.close()
         logging.info(f'{month_pattern} Q Connection. Is connected: {q.is_connected()}')
-        q_process.close()
-        logging.info(f'{month_pattern} stopped Q Process on port {port_num}.')
+        # q_process.close()
 
 
 def process_month_trades(logger, month_pattern, data_path, temp_dir=EXTRACT_DIR):
-    
     qh = QueueHandler(logger)
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
@@ -203,9 +203,9 @@ def process_month_trades(logger, month_pattern, data_path, temp_dir=EXTRACT_DIR)
     
     try:
         q_process = os.popen(os.path.join(Q_DIR, 'q').replace("\\", "/") + f' -p {port_num}')
-            # Popen([os.path.join(Q_DIR, 'q').replace("\\", "/"), f' -p {port_num}']) # -s {N_Q_THREADS}
+        # Popen([os.path.join(Q_DIR, 'q').replace("\\", "/"), f' -p {port_num}']) # -s {N_Q_THREADS}
         time.sleep(1)
-    
+        
         # let us connect to that Q process
         q = qconnection.QConnection(host='localhost', port=port_num)
         # initialize connection (make sure it is multi-threaded startup)
@@ -230,6 +230,7 @@ def process_month_trades(logger, month_pattern, data_path, temp_dir=EXTRACT_DIR)
         q_process.close()
         logging.info(f'{month_pattern} stopped Q Process on port {port_num}.')
 
+
 def main():
     logging.basicConfig(filename=os.path.join(LOG_DIR, f"upload_kdb_main_{datetime.now().strftime('%Y%m%dT%H%M%S')}.log").replace("\\", "/"),
                         filemode='a',
@@ -242,7 +243,7 @@ def main():
     # from 201705 201902 => for if we want to try this multiprocessing
     starting_month_patterns = [x.strftime("%Y%m") for x in list(pd.date_range("2017-05-01", "2019-03-01", freq='M'))]
     root = 'D:/data/m_data/'
-    dirs_to_load = ['EUX'] #, 'ETF', 'MTA']
+    dirs_to_load = ['EUX']  # , 'ETF', 'MTA']
     
     for dir_to_load in dirs_to_load:
         data_path = os.path.join(root, dir_to_load).replace("\\", "/")
@@ -266,73 +267,14 @@ def main():
             for p in ps:
                 p.join()
             logging.info(f'Finished loading books from {data_path}')
-
+    
     log_q_listener.stop()
     
     logging.info('Finished')
 
+
 if __name__ == '__main__':
     main()
-    
-    
-    
-    # n_processes = multiprocessing.cpu_count()
-    # book_gz_csv_files = []
-    # trade_gz_csv_files = []
-    # book_patterns_suffix = "[0-9]{2}_\S+_Book.csv.gz$"  # [re.compile("^" + x + "[0-9]{2}_\S+_Book.csv.gz$") for x in starting_month_patterns]
-    # trade_patterns_suffix = "[0-9]{2}_\S+_MKtrade.csv.gz$"  # [re.compile("^" + x + "[0-9]{2}_\S+_MKtrade.csv.gz$") for x in starting_month_patterns]
-    # qty_pattern = re.compile("^.*_Qty_Lev_[0-9]$")
-    # px_pattern = re.compile("^.*_Px_Lev_[0-9]$")
-    
-    # for month_pattern in starting_month_patterns:
-    #
-    #     book_pattern = re.compile("^" + month_pattern + "[0-9]{2}_\S+_Book.csv.gz$")
-    #     trade_pattern = re.compile("^" + month_pattern + "[0-9]{2}_\S+_MKtrade.csv.gz$")
-    #
-    #     # make sure the file patterns are mutually exclusive, else you will be loading multiple times the same data
-    #     book_gz_csv_files = [x for x in os.listdir(data_path) if book_pattern.match(x) is not None]
-    #     trade_gz_csv_files = [x for x in os.listdir(data_path) if trade_pattern.match(x) is not None]
-    #
-    #     if not book_gz_csv_files:
-    #         logging.error(f"No Book files found for pattern {month_pattern} in directory: {data_path}")
-    #     if not trade_gz_csv_files:
-    #         logging.error(f"No Trade files found  for pattern {month_pattern} in directory: {data_path}")
-    #     if not trade_gz_csv_files and not book_gz_csv_files:
-    #         continue
-    #
-    #     logging.info(f"Found book files {str(book_gz_csv_files)}")
-    #     logging.info(f"Found trade files {str(trade_gz_csv_files)}")
-    #
-    #     book_gz_csv_files.sort()
-    #     trade_gz_csv_files.sort()
-    #
-    #     for zipped_trade_filename in trade_gz_csv_files:
-    #         process_trade_file(zipped_trade_filename, parent_dir=data_path, temp_dir="I:/testkdb/tempdir")
-    #
-    #     # tag = dir_to_load.strip().upper()
-    #     # func = partial(read_and_persist, data_path=data_path, qty_pattern=qty_pattern, px_pattern=px_pattern,
-    #     #                tag=tag, lib=lib, action=arctic_action)
-    #
-    #     # with multiprocessing.Pool(processes=n_processes) as pool:
-    #     #    pool.map(func, book_gz_csv_files[:10])
-    #     # for gz_csv_file in book_gz_csv_files:
-    #     #     func(gz_csv_file)
 
-
-
-
-# trades:("SDIIIIFI";enlist "|") 0: `:I:/beetroot/csv_data_from_py/20170505_EUX_MKtrade.csv;
-# update sym:ISIN, time: (1000*TimeMM) + Date + `second$(60*60*TimeSec div 10000) + (60 * (TimeSec mod 10000) div 100) + (TimeSec mod 100) from `trades;
-# delete from `trades where Price=0;
-# delete ISIN, TimeMM, Date, TimeSec, MarketTime from `trades;
-# trades:`sym`time`Price`Qty`Volume xcols tab;
-# .Q.dpft[`:I:/testkdb/trades/;2017.05.05;`sym;`trades];
-# delete trades from `.;
-# \l I:/testkdb/trades/2017.05.05/;
-# \a;
-# show[trades]
-#
-# meta trades
-# select from trades where Qty >10
-# tab:("SDIIFFFFFFFFFFFFFFFFFFFF";enlist "|") 0: `:I:/beetroot/csv_data_from_py/20170505_EUX_Book.csv
-# update TimeSpan: (1000*TimeMM) + Date + `second$(60*60*TimeSec div 10000) + (60 * (TimeSec mod 10000) div 100) + (TimeSec mod 100) from `tab
+# \l I:/testkdb/trades/;
+# exec count[select from trades where date=2017.06.02];
